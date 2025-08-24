4b14db453d0c0e36f56a8369a98d650ec3fdde66
Yuvrajkeshav071 <159120223+Yuvrajkeshav071@users.noreply.github.com>
2025-08-24 18:29:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -59,8 +59,9 @@ app.post("/edit/:id", (req, res) => {
   res.redirect("/");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
